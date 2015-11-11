e563f64e6323b36c7f9f112617c6067ddf893f40
beidelss <beidelss@onid.oregonstate.edu>
2015-11-10 21:44:14 -0500

    added views

--- randNum.js
+++ randNum.js
@@ -1,13 +1,19 @@
 var express = require('express');
 
 var app = express();
+var handlebars = require("express-handlebars").create({defaultLayout:"main"});
 
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
 app.set('port', 3000);
 
+function getRand() {
+	var randNum = {};
+	randNum.stringNum = String(Math.random());
+	return randNum;
+}
 app.get('/', function(req, res) {
-  res.type('text/plain');
-  var num = Math.random();
-  res.send(String(num));
+    res.render("stringNum", getRand());
 });
 
 app.use(function(req, res) {
@@ -26,4 +32,4 @@ app.use(function(err, req, res, next) {
 app.listen(app.get('port'), function() {
   console.log('Random Num started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
-~
+
